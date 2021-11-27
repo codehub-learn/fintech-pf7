0ca1683e8fd1aa2273e77a6d50e28ce8a4a804e4
Ioannis Daniil <jondan97@gmail.com>
2021-11-27 19:46:42 +0200

    Added validations to domain

--- java-path-spring-commons/src/main/java/gr/codelearn/domain/Account.java
+++ java-path-spring-commons/src/main/java/gr/codelearn/domain/Account.java
@@ -8,6 +8,9 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -22,8 +25,14 @@ public class Account implements Serializable {
     @Id
     @GeneratedValue
     private UUID id;
+    @NotNull(message = "Name cannot be null.")
     private String name;
+    @Size(min = 16, max = 16, message = "IBAN should be of length 16.")
+    @NotNull(message = "IBAN cannot be null.")
     private String iban;
+    @NotNull(message = "Type cannot be null.")
+    // This should also have a check for whether the type is acceptable or not
     private AccountType type;
+    @PositiveOrZero(message = "Balance cannot be below zero.")
     private BigDecimal balance;
 }
--- java-path-spring-commons/src/main/java/gr/codelearn/domain/Payment.java
+++ java-path-spring-commons/src/main/java/gr/codelearn/domain/Payment.java
@@ -1,14 +1,17 @@
 package gr.codelearn.domain;
 
+import gr.codelearn.validation.CurrencyType;
+import gr.codelearn.validation.DateFormatted;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
-import java.util.UUID;
 
 @Data
 @Builder
@@ -16,13 +19,24 @@ import java.util.UUID;
 @NoArgsConstructor
 // AMQP requires serializable
 public class Payment implements Serializable {
-    private UUID id;
+    @NotNull(message = "Creditor cannot be null.")
     private Account creditor;
+    @NotNull(message = "Debtor cannot be null.")
     private Account debtor;
+    @NotNull(message = "Value Date cannot be null.")
+    @DateFormatted(pattern = "yyyyMMdd", message = "Date was not passed correctly.")
+    // Does not 100% work, "-" are parsed if placed at particular places
     private Date valueDate;
+    @NotNull(message = "Payment amount cannot be null.")
+    @Positive(message = "Payment amount needs to be positive.")
     private BigDecimal paymentAmount;
+    @CurrencyType(anyOf = {Currency.EUR}, message = "Must be any of the accepted types (e.g., EUR)")
+    @NotNull(message = "Payment currency cannot be null.")
     private Currency paymentCurrency;
-    // todo should this be defined in here?
+    @Positive(message = "Fee needs to be positive.")
+    @NotNull(message = "Fee amount cannot be null.")
     private BigDecimal feeAmount;
+    @CurrencyType(anyOf = {Currency.EUR}, message = "Must be any of the accepted types (e.g., EUR)")
+    @NotNull(message = "Fee currency cannot be null.")
     private Currency feeCurrency;
 }
